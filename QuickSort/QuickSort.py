7bcb135f3de8765d8352b9a1fd8181705dc7ee97
matejpopovski <matej.popovski@gmail.com>
2024-02-25 00:30:26 -0600

    Quick Sort

--- QuickSort/QuickSort.py
+++ QuickSort/QuickSort.py
@@ -1,11 +1,3 @@
-# Python program for implementation of Quicksort Sort
-
-# This implementation utilizes pivot as the last element in the nums list
-# It has a pointer to keep track of the elements smaller than the pivot
-# At the very end of partition() function, the pointer is swapped with the pivot
-# to come up with a "sorted" nums relative to the pivot
-
-
 # Function to find the partition position
 def partition(array, low, high):
 
