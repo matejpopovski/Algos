3e7e5a8d25941c852ea6dd52fb3b95b75d8fb0f6
matejpopovski <matej.popovski@gmail.com>
2025-04-16 04:15:33 -0500

    pygame practice

--- connect_4/connect_4.py
+++ connect_4/connect_4.py
@@ -20,6 +20,24 @@ screen = pygame.display.set_mode(WINDOW_SIZE)
 pygame.display.set_caption("Connect 4")
 clock = pygame.time.Clock()
 
+def draw_board():
+    screen.fill(BG_COLOR)
+    for row in range(ROWS):
+        for col in range(COLS):
+            x = col * CELL_SIZE + CELL_SIZE // 2
+            y = row * CELL_SIZE + CELL_SIZE // 2
+            pygame.draw.circle(screen, CIRCLE_COLOR, (x, y), CIRCLE_RADIUS)
+    pygame.display.update()
+
+def get_column_from_click(pos):
+    x, _ = pos
+    col = x // CELL_SIZE
+    return col
+
+
+
+
+
 running = True
 while running:
     clock.tick(FPS)
@@ -30,11 +48,10 @@ while running:
         
         if event.type == pygame.MOUSEBUTTONDOWN:
             pos = pygame.mouse.get_pos()
-            print(f"Mouse clicked at: {pos}")
+            col = get_column_from_click(pos)
+            print(f"Clicked on column: {col}")
     
-    screen.fill((30, 30, 60))
-    
-    pygame.display.flip()
+    draw_board()
 
 pygame.quit()
 sys.exit()
